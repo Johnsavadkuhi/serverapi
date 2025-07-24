11d8ecc270e8f6fced47893b05a0657565ac2928
Johnsavadkuhi <physitime@gmail.com>
2025-07-25 01:09:39 +0330

    add

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,13 +1,78 @@
 const express = require('express');
-const { getUserProjects, getManagerProjects, createProject , getBugs , updateBugStatus, updateBulkBugStatus } = require('../controllers/projectControllers');
+const { getUserProjects, getManagerProjects, createProject , getBugs , updateBugStatus, updateBulkBugStatus,
+    creatReport,fetchReport , updateReport , fetchAllReports  , fetchProjectById 
+ } = require('../controllers/projectControllers');
+
+
 
 const router = express.Router();
+
+const multer  = require("multer")
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs") 
+const path = require("path")
+
+
+
+
+
+  const pocStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      
+    //   const bugLabel = req.body.label || "unknown"; // Default label if none is provided
+      const { projectId , label:bugLabel  } = req.body ; // Get project ID from route params
+  
+      // Base project directory
+      const projectDir = `upload/${projectId}`;
+      // Subdirectory for bug label
+      const bugDir = `${projectDir}/${bugLabel}`;
+  
+      // Ensure project directory exists
+      if (!fs.existsSync(projectDir)) {
+        fs.mkdirSync(projectDir, { recursive: true });
+      }
+  
+      // Ensure bug label directory exists
+      if (!fs.existsSync(bugDir)) {
+        fs.mkdirSync(bugDir, { recursive: true });
+      }
+  
+      // Save projectId and bugLabel for future use
+      req.projectId = projectId;
+      req.bugLabel = bugLabel;
+  
+      // Set the upload directory to the bug label directory
+      cb(null, bugDir);
+    },
+    filename: (req, file, cb) => {
+      const fileExtension = path.extname(file.originalname);
+
+      const uniqueName = `${uuidv4()}${fileExtension}`; // Generate a unique file name
+      cb(null, uniqueName);
+    },
+  });
+  
+
+
+
+const pocUpload = multer({storage:pocStorage}); 
+
+
 
 router.get('/user',getUserProjects);
 router.get("/manager" ,getManagerProjects )
 router.post("/devops/create" , createProject)
-router.get("/bugs" ,getBugs )
+
 router.post("/update/bug/status" , updateBugStatus)
 router.post("/bulk/update/bug/status" , updateBulkBugStatus)
+router.get("/bugs" ,getBugs )
+// router.post("/bug/creat/report" , creatReport)
+router.route("/bug/creat/report").post(pocUpload.array('files', 100) , creatReport)
+.put(pocUpload.array('files' , 100) , updateReport)
+router.get("/user/report" , fetchReport)
+router.get("/reports/all/users" , fetchAllReports)
+router.get("/project" , fetchProjectById)
+
+
 module.exports = router; 
  
