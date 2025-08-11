4954e1ebfe0a3ab3dd323b3233871449eb1f095f
Johnsavadkuhi <physitime@gmail.com>
2025-08-11 11:32:47 +0330

    update

--- src/routes/devopsRoutes.js
+++ src/routes/devopsRoutes.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const { getDevopsProjects ,
-     getDevopsProject ,
-     updateDevopsProject, 
-    deleteDevopsProject ,getDevopsProjectPentesters,
-registerDevOpsInfo , getDevOpsInfo } = require('../controllers/devopsController');
+const { getDevopsProjects,
+    getDevopsProject,
+    updateDevopsProject,
+    deleteDevopsProject,
+     getDevopsProjectPentesters,
+    registerDevOpsInfo,
+    getDevOpsInfo,
+    getProjectUrl } = require('../controllers/devopsController');
 
 const router = express.Router();
 
-router.get('/projects',getDevopsProjects);
-router.get('/project',getDevopsProject);
-router.get('/project/pentesters',getDevopsProjectPentesters);
-router.post('/update/project' , updateDevopsProject)
-router.delete('/delete/project' , deleteDevopsProject)
-router.post("/project/save/info" , registerDevOpsInfo)
-router.get("/info" , getDevOpsInfo)
+router.get('/projects', getDevopsProjects);
+router.get('/project', getDevopsProject);
+router.get('/project/pentesters', getDevopsProjectPentesters);
+router.post('/update/project', updateDevopsProject)
+router.delete('/delete/project', deleteDevopsProject)
+router.post("/project/save/info", registerDevOpsInfo)
+router.get("/info", getDevOpsInfo)
+router.get("/endpoint/url", getProjectUrl)
 module.exports = router; 
