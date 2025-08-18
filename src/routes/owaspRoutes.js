ef656b288a39e768a1862fd12b6b8d87182b893e
Johnsavadkuhi <physitime@gmail.com>
2025-08-18 08:35:18 +0330

    update

--- src/routes/owaspRoutes.js
+++ src/routes/owaspRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { saveWebOwaspWstg , getAllOwaspWeb} = require('../controllers/owaspController');
+const { saveWebOwaspWstg , getAllOwaspWeb , saveImpactsWeb , getOwaspItemById } = require('../controllers/owaspController');
 
 const router = express.Router();
 
 router.get('/save/web', saveWebOwaspWstg);
 // router.post("/mobile" , registerUser)
 router.get('/web' ,getAllOwaspWeb )
+router.get("/save/impacts/web" , saveImpactsWeb)
+router.get("/impact" , getOwaspItemById)
 module.exports = router;
