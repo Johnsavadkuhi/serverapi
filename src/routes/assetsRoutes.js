dfeb2ad2131fa980544159fecfea2d737cab2c9c
Johnsavadkuhi <physitime@gmail.com>
2025-10-22 13:44:48 +0330

    add new routes

--- src/routes/assetsRoutes.js
+++ src/routes/assetsRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { addAsset , getAssets, getAsset } = require('../controllers/assetsController');
+const { addAsset , getAssets, getAsset , updateAsset, deleteAsset } = require('../controllers/assetsController');
 const router = express.Router();
 
 
 router.post("/create" , addAsset)
+router.post("/update" , updateAsset)
 router.post("/" ,getAssets)
 router.get("/details" , getAsset)
+router.post("/delete" , deleteAsset)
 module.exports = router;
